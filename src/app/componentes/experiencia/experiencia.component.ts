69a1ffb9dcb37f805e8fd9d0a9eabff176c68fab
Marianela <gaia.marianela@gmail.com>
2022-07-12 22:31:46 -0300

    bootstrap-experiencia

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Experiencia } from 'src/app/modelos/experiencia';
+import { ExperienciaService } from 'src/app/servicios/experiencia.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -7,9 +11,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienciaComponent implements OnInit {
 
-  constructor() { }
+  public experiencias:Experiencia[]=[];
+  public actualizarExperiencia:Experiencia | undefined;
+  public eliminarExperiencia:Experiencia | undefined;
+  
+  constructor(private experienciaService:ExperienciaService) { }
 
   ngOnInit(): void {
+    this.obtenerExperiencias();  
   }
+
+  public obtenerExperiencias():void {
+    this.experienciaService.obtenerExperiencia().subscribe({
+      next: (Response: Experiencia[]) => {
+        this.experiencias=Response;
+      },
+      error:(error:HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
+
+  public onOpenModal (mode:String, experiencia?: Experiencia):void {
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.type = 'button';
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode==='agregar') {
+      button.setAttribute('data-target', '#agregarExperienciaModal');
+    } else if (mode==='eliminar') {
+      this.eliminarExperiencia=experiencia;
+      button.setAttribute('data-target', '#eliminarExperienciaModal');
+    } else if (mode==='actualizar') {
+      this.actualizarExperiencia=experiencia;
+      button.setAttribute('data-target', '#actualizarExperienciaModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onAddExperiencia(addForm: NgForm) {
+    document.getElementById('add-experience-form')?.click();
+    this.experienciaService.agregarExperiencia(addForm.value).subscribe ({
+    next: (response:Experiencia) => {
+      console.log(response);
+      this.obtenerExperiencias();
+      addForm.reset();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+      addForm.reset();
+    }
+    })
+  }
+
+  public onUpdateExperiencia(experiencia: Experiencia) {
+    this.actualizarExperiencia=experiencia;
+    document.getElementById('add-experience-form')?.click();
+    this.experienciaService.actualizarExperiencia(experiencia).subscribe ({
+    next: (response:Experiencia) => {
+      console.log(response);
+      this.obtenerExperiencias();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+    }
+  })
+  }
+
+  public onDeleteExperiencia(idEx: number): void {
+    this.experienciaService.eliminarExperiencia(idEx).subscribe ({
+    next: (response:void) => {
+      console.log(response);
+      this.obtenerExperiencias();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+    }
+  })
+  }
+
 
 }
--- src/app/servicios/experiencia.service.ts
+++ src/app/servicios/experiencia.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Experiencia } from '../modelos/experiencia';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienciaService {
+  private apiServerUrl=environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public obtenerExperiencia():Observable<Experiencia[]>{
+    return this.http.get<Experiencia[]>(`${this.apiServerUrl}/experiencia/obtener`);
+  }
+
+  public agregarExperiencia(experiencia : Experiencia):Observable<Experiencia>{
+    return this.http.post<Experiencia>(`${this.apiServerUrl}/experiencia/agregar`,experiencia);
+  }
+
+  public actualizarExperiencia(experiencia : Experiencia):Observable<Experiencia>{
+    return this.http.put<Experiencia>(`${this.apiServerUrl}/experiencia/actualizar`,experiencia);
+  }
+
+  public eliminarExperiencia(experienciaId : number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/experiencia/eliminar/${experienciaId}`);
+  }
 }
