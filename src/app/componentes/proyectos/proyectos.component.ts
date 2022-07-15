189adce407892fdb6d9a3600ce36b17a931c6d50
Marianela <gaia.marianela@gmail.com>
2022-07-14 21:17:35 -0300

    angular-proyectos

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,15 +1,97 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Proyectos } from 'src/app/modelos/proyectos';
+import { ProyectosService } from 'src/app/servicios/proyectos.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
+
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  public proyectos:Proyectos[]=[];
+  public actualizarProyectos:Proyectos | undefined;
+  public eliminarProyectos:Proyectos | undefined;
+  
+  constructor(private proyectosService:ProyectosService) { }
 
   ngOnInit(): void {
+    this.obtenerProyecto();  
+  }
+
+  public obtenerProyecto():void {
+    this.proyectosService.obtenerProyecto().subscribe({
+      next: (Response: Proyectos[]) => {
+        this.proyectos=Response;
+      },
+      error:(error:HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
+
+  public onOpenModal (mode:String, proyecto?: Proyectos):void {
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.type = 'button';
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode==='agregar') {
+      button.setAttribute('data-target', '#agregarProyectoModal');
+    } else if (mode==='eliminar') {
+      this.eliminarProyectos=proyecto;
+      button.setAttribute('data-target', '#eliminarProyectoModal');
+    } else if (mode==='actualizar') {
+      this.actualizarProyectos=proyecto;
+      button.setAttribute('data-target', '#actualizarProyectoModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onAddProyecto(addForm: NgForm) {
+    document.getElementById('add-project-form')?.click();
+    this.proyectosService.agregarProyecto(addForm.value).subscribe ({
+    next: (response:Proyectos) => {
+      console.log(response);
+      this.obtenerProyecto();
+      addForm.reset();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+      addForm.reset();
+    }
+    })
+  }
+
+  public onUpdateProyecto(proyecto: Proyectos) {
+    this.actualizarProyectos=proyecto;
+    document.getElementById('add-project-form')?.click();
+    this.proyectosService.actualizarProyecto(proyecto).subscribe ({
+    next: (response:Proyectos) => {
+      console.log(response);
+      this.obtenerProyecto();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+    }
+  })
+  }
+
+  public onDeleteProyecto(idProy: number): void {
+    this.proyectosService.eliminarProyecto(idProy).subscribe ({
+    next: (response:void) => {
+      console.log(response);
+      this.obtenerProyecto();
+    },
+    error:(error:HttpErrorResponse) => {
+      alert(error.message);
+    }
+  })
   }
 
 }
--- src/app/servicios/proyectos.service.ts
+++ src/app/servicios/proyectos.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Proyectos } from '../modelos/proyectos';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProyectosService {
 
-  constructor() { }
+export class ProyectosService {
+  private apiServerUrl=environment.apiBaseUrl;
+
+  constructor(private http:HttpClient) { }
+
+  public obtenerProyecto():Observable<Proyectos[]>{
+    return this.http.get<Proyectos[]>(`${this.apiServerUrl}/proyectos/obtener`);
+  }
+
+  public agregarProyecto(proyecto : Proyectos):Observable<Proyectos>{
+    return this.http.post<Proyectos>(`${this.apiServerUrl}/proyectos/agregar`,proyecto);
+  }
+
+  public actualizarProyecto(proyecto : Proyectos):Observable<Proyectos>{
+    return this.http.put<Proyectos>(`${this.apiServerUrl}/proyectos/actualizar`,proyecto);
+  }
+
+  public eliminarProyecto(proyectoId : number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/proyectos/eliminar/${proyectoId}`);
+  }
+
 }
